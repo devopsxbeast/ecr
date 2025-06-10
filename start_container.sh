80b19391bb5dbded3886fd4c231aa1b79d8b36f1
devopsxbeast <thodahatke0@gmail.com>
2025-06-10 16:35:10 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull devopsxbeast09/ecr
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 727646469467.dkr.ecr.us-east-1.amazonaws.com/updated
+# Pull the Docker image from ECR
+docker pull 727646469467.dkr.ecr.us-east-1.amazonaws.com/updated:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 devopsxbeast09/ecr
+docker run -d -p 5000:5000 727646469467.dkr.ecr.us-east-1.amazonaws.com/updated:latest
